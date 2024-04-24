fd5c3f8909e7ddf44eb4ff976110df6b0c264a5f
wahid khan <wahidkhanhec07@gmail.com>
2024-04-24 15:19:27 +0500

    new commit

--- lib/drawer/About/about_section.dart
+++ lib/drawer/About/about_section.dart
@@ -1,7 +1,5 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:hayat_e_sahaba/drawer/theme_changer/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
